7072a9a804b90a93559ba8e948466c372f1059ff
StpehenChinag <stephenchinag@gmail.com>
2023-10-16 10:06:15 +0100

    framepRO

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import React, {useState} from 'react';
-
-import './App.css';
-import Button from './components/UI/Button/Button';
-import Demmo from './components/UI/Button/Demo/Demmo';
-
 function App() {
-  const [showForNow , setShowForNow ]=useState(false)
-  const ClickMeHandler=()=>{
-    setShowForNow((prevShow)=>!prevShow)
-  }
-
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-     <Demmo show={showForNow}/>
-      <Button onClick={ClickMeHandler}>Click Me </Button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/UI/Button/Demo/Demmo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
- const Demmo=(props)=> {
-  return (
-    <div>{props.show ?'hello is ':'' }</div>
-  )
-}
-export default Demmo;
